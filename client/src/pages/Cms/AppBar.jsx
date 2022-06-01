b7ec69a3576af9843420f4e045f09d8bd1737511
Ajoscha <driemel.a@gmail.com>
2022-06-01 11:45:12 +0200

    added navbar collapse

--- client/src/pages/Cms/AppBar.jsx
+++ client/src/pages/Cms/AppBar.jsx
@@ -10,21 +10,12 @@ export default function AppBar() {
       x.style.display = "block";
     }
   }
-  /* const [show, setShow] = useState(true); */
   return (
     <div>
     <div className="appbar">
-     {/*  <div className="hamburger" onClick={() => setShow((prev) => !prev)}>
-        <svg viewBox="0 0 100 80" width="40" height="40" fill="white">
-          <rect width="100" height="15" ></rect>
-          <rect y="30" width="100" height="15"></rect>
-          <rect y="60" width="100" height="15"></rect>
-        </svg>
-      </div>   */}
+     
         <button onClick={function1}>Click me</button>
       </div>
-      {/* {show && <Navbar />} */}
-      
     </div>
   );
 }
