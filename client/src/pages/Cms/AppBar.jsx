14ab52111369904a3be59275b2bd10dd4db08c0b
admin <dave.rudschewski@gmx.de>
2022-06-01 16:23:32 +0200

    some more minor changes...

--- client/src/pages/Cms/AppBar.jsx
+++ client/src/pages/Cms/AppBar.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function AppBar() {
-  const function1 = function myFunction() {
-    var x = document.getElementById("nav-container");
-    if (x.style.display === "block") {
-      x.style.display = "none";
+  const [navbarDisplay, setNavbarDisplay] = useState("block");
+
+  const toggleNavbar = () => {
+    const navbar = document.getElementById("nav-container");
+    if (navbar.style.display === "block") {
+      navbar.style.display = "none";
     } else {
-      x.style.display = "block";
+      navbar.style.display = "block";
     }
   };
 
@@ -14,7 +16,7 @@ export default function AppBar() {
     <div>
       <div className="appbar">
         <div className="hamburger">
-          <svg viewBox="0 0 100 80" width="40" height="40" fill="#FFFFFF" onClick={function1}>
+          <svg viewBox="0 0 100 80" width="40" height="40" fill="#FFFFFF" onClick={toggleNavbar}>
             <rect width="100" height="15"></rect>
             <rect y="30" width="100" height="15"></rect>
             <rect y="60" width="100" height="15"></rect>
