72d0166811c29d538273898090b06a78032a202b
Ajoscha <driemel.a@gmail.com>
2022-06-01 15:28:18 +0200

    login

--- client/src/pages/Cms/Cms.jsx
+++ client/src/pages/Cms/Cms.jsx
@@ -1,25 +1,67 @@
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "../../components/Dashboard/Dashboard";
 import MemberList from "../../components/MemberList/MemberList";
 import AppBar from "./AppBar";
 import Navbar from "./Navbar";
-import "./cms.css"
+import "./cms.css";
 import Calendar from "../../components/Calendar/calendar";
 import Weather from "../../components/Weather/Weather";
 import InfoPage from "../../components/InfoPage/InfoPage";
+import LoginForm from "../../components/LoginForm/LoginForm";
 
 export default function Cms() {
+  const adminUser = {
+    email: "admin@admin",
+    password: "admin",
+  };
+
+  const [user, setUser] = useState({ name: "", email: "" });
+  const [error, setError] = useState("");
+
+  const Login = (details) => {
+    console.log(details);
+
+    if (
+      details.email == adminUser.email &&
+      details.password == adminUser.password
+    ) {
+      console.log("Logged in");
+      setUser({
+        name: details.name,
+        email: details.email,
+      });
+    } else {
+      console.log("Details do not Match!");
+      setError("Details do not Match!");
+    }
+  };
+
+  const Logout = () => {
+    setUser({ name: "", email: "" });
+  };
+
   return (
-    <>
-      <AppBar />
-      <Navbar />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="memberList" element={<MemberList />} />
-          <Route path="calendar" element={<Calendar />} />
-          <Route path="weather" element={<Weather />} />
-          <Route path="infopage" element={<InfoPage />} />
-        </Routes> 
-    </>
+    <div>
+      {user.email !== "" ? (
+        <div>
+          <h2>
+            Welcome back, <span>{user.name}</span>
+          </h2>
+          <button onClick={Logout}>Logout</button>
+          <AppBar />
+        <Navbar />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="memberList" element={<MemberList />} />
+            <Route path="calendar" element={<Calendar />} />
+            <Route path="weather" element={<Weather />} />
+            <Route path="infopage" element={<InfoPage />} />
+          </Routes> 
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error} />
+      )}
+    </div>
   );
 }
