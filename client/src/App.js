3b99b927be6e0890fe57b8c72e3df2c1101dd6c1
Ajoscha <driemel.a@gmail.com>
2022-06-07 11:53:25 +0200

    backend and memberlist

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,60 @@
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import Axios from "axios";
 
 export default function App() {
+  const [listOfMembers, setListOfMembers] = useState([]);
+  const [name, setName] = useState("");
+  const [age, setAge] = useState(0);
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/getMembers").then((response) => {
+      setListOfMembers(response.data);
+    });
+  }, []);
+
+  const createMember = () => {
+    Axios.post("http://localhost:3001/createMember", {
+      name: name,
+      age: age,
+    }).then((response) => {
+      setListOfMembers([...listOfMembers, { name: name, age:age }]);
+      /* alert("MEMBER CREATED "); */
+    });
+  };
+
   return (
-    <div>
-      <h1>Bookkeeper!</h1>
+    <div className="App">
+      <h2>Member List</h2>
+      <div className="memberDisplay">
+        {listOfMembers.map((member) => {
+          return (
+            <div>
+              <p>Name: {member.name}</p>
+              <p>Age: {member.age}</p>
+              <br />
+            </div>
+          );
+        })}
+
+        <div>
+          <input
+            type="text"
+            placeholder="Name... "
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+          />
+          <input
+            type="number"
+            placeholder="Age... "
+            onChange={(event) => {
+              setAge(event.target.value);
+            }}
+          />
+          <button onClick={createMember}>Create Member</button>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ server/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const MemberModel = require("./models/members")
+
+const cors = require('cors')
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(
+  "mongodb+srv://admin:admin@cluster0.rxyt27g.mongodb.net/scms?retryWrites=true&w=majority"
+);
+
+
+app.get("/getMembers", (req, res) => {
+    MemberModel.find({}, (err, result) => {
+        if (err) {
+            res.json(err);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+app.post("/createMember", async (req, res) => {
+  const member = req.body;
+  const newMember = new MemberModel(member);
+  await newMember.save();
+
+  res.json(member);
+});
+
+app.listen(3001, () => {
+  console.log("listen on port 3001");
+});
+
+
+/* 38 minutes - https://www.youtube.com/watch?v=I7EDAR2GRVo */
